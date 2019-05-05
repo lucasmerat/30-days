2ab073e2643b4a75d0465f08317a4f6a6d63c511
Randolph Bailey <randolphbailey@gmail.com>
2019-05-05 09:12:36 -0500

    set up react and deployment configuration

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// API calls
+app.get("/api/hello", (req, res) => {
+  res.send({ express: "Hello From Express" });
+});
+app.post("/api/world", (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`
+  );
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
