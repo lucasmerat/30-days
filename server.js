053be044b09daed2c9edaa06c9a89b0965f8f147
Randolph Bailey <randolphbailey@gmail.com>
2019-05-06 17:14:12 -0500

    init mongo server and connect.  Start integrating passport for Instagram auth

--- server.js
+++ server.js
@@ -3,14 +3,58 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const InstagramStrategy = require("passport-instagram").Strategy;
+const util = require("util");
+const INSTAGRAM_CLIENT_ID = process.env.CLIENT_ID;
+const INSTAGRAM_CLIENT_SECRET = process.env.CLIENT_SECRET;
+const MONGO_PW = process.env.MONGO_PW;
 const port = process.env.PORT || 5000;
 
-var app = express();
+//Connect to MongoDB
+mongoose.connect("mongodb://srv-captain--project-3-db:27017/project3", {
+  useNewUrlParser: true
+});
+
+//Initialize Passport and Instagram Authentication
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(
+  new InstagramStrategy(
+    {
+      clientID: INSTAGRAM_CLIENT_ID,
+      clientSecret: INSTAGRAM_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/auth/instagram/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function() {
+        // To keep the example simple, the user's Instagram profile is returned to
+        // represent the logged-in user.  In a typical application, you would want
+        // to associate the Instagram account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
+  )
+);
+
+//Initialize Express Server
+const app = express();
 
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // API calls
 app.get("/api/hello", (req, res) => {
