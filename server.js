771019c36680cdfa16cc44eea7d000d6b9e872b4
Randolph Bailey <randolphbailey@gmail.com>
2019-05-08 15:16:53 -0500

    fix login redirect

--- server.js
+++ server.js
@@ -36,17 +36,6 @@ app.get("/login", function(request, response) {
   response.redirect(process.env.INSTAGRAM_AUTH_URL);
 });
 
-// API calls
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-app.post("/api/world", (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`
-  );
-});
-
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
