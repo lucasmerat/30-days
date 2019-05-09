0b46253f01f8db3499881bc8c9d9f57af0a648c4
Felix Villalobos <felix.evl@gmail.com>
2019-05-09 12:02:06 -0500

    Change in models and added some routes

--- models/Post.js
+++ models/Post.js
@@ -5,11 +5,24 @@ var Schema = mongoose.Schema;
 var PostSchema = new Schema({
   title:String,
   body:String,
-  image:String,
+  currentDay:{
+    type:Number,
+    default:0},
+  totalDays:{
+    type:Number,
+    default:30},
   createdAt:{
     type:Date,
     default:Date.now
   },
+  challenge: [{
+    type: Schema.Types.ObjectId,
+    ref: "Challenge"
+  }],
+  user: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }]
 });
 
 
