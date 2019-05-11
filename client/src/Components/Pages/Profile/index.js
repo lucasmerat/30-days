8606db1008005421c0d12c36fb0149a32718a8cb
Lucas Merat <lucas.merat@gmail.com>
2019-05-11 11:36:51 -0500

    Class-based component for Profile with state, passing props to each route

--- client/src/Components/Pages/Profile/index.js
+++ client/src/Components/Pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "./style.css";
 import ChallengesNav from "../ChallengesNav";
 import ProfileNav from "../ProfileNav";
@@ -7,28 +7,35 @@ import OngoingChallenges from "../OngoingChallenges";
 import DoneChallenges from "../DoneChallenges";
 import Timeline from "../Timeline";
 import CreateChallenges from "../CreateChallenges";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-function profile(props) {
-  console.log(props.location.pathname)
-  return (
-    <div>
-      <ChallengesNav />
-      <ProfileNav />
-        <Switch>
-          <Route path={'/profile/browse'} component={BrowseChallenges} />
-          <Route exact path={'/profile/ongoing'} component={OngoingChallenges}></Route>
-          <Route exact path={'/profile/done'} component={DoneChallenges}></Route>
-          <Route exact path={'/profile/timeline'} component={Timeline}></Route>
-          <Route exact path={'/profile/create'} component={CreateChallenges}></Route>
-        </Switch>
-      <div className="row">
-        <div className="col-4" />
-        <div className="col-8">
+class Profile extends Component {
+  state = {
+    challenges: []
+  }
+  componentDidMount(){
+    console.log("It mounted")
+  }
+  render(){
+    return (
+      <div>
+        <ChallengesNav />
+        <ProfileNav />
+          <Switch>
+            <Route path={'/profile/browse'} render={(props) => <BrowseChallenges {...props} challenges={this.state.challenges} />} />
+            <Route path={'/profile/ongoing'} component={OngoingChallenges}></Route>
+            <Route path={'/profile/done'} component={DoneChallenges}></Route>
+            <Route path={'/profile/timeline'} component={Timeline}></Route>
+            <Route path={'/profile/create'} component={CreateChallenges}></Route>
+          </Switch>
+        <div className="row">
+          <div className="col-4" />
+          <div className="col-8">
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default profile;
+export default Profile;
