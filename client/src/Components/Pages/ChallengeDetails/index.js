ab6001c4285e914d3998a8ed5108c51400adb8d5
Lucas Merat <lucas.merat@gmail.com>
2019-05-13 11:37:44 -0500

    Basic layout of rendered workout... ready for API call

--- client/src/Components/Pages/ChallengeDetails/index.js
+++ client/src/Components/Pages/ChallengeDetails/index.js
@@ -5,12 +5,12 @@ import "./ChallengeDetails.css";
 class ChallengeDetails extends Component {
   componentDidMount() {
     this.loadChallenge();
-  };
+  }
   state = {
     title: "Workout title",
     description: "Workout description",
-    days: "",
-    numUsers: null
+    days: ["Workout 1", "Workout2", "Workout3", "...... more workout days"],
+    numUsers: 100
   };
   loadChallenge = () => {
     console.log("API call here to load the challenge data");
@@ -22,10 +22,20 @@ class ChallengeDetails extends Component {
         <div className="card challenge-details-card">
           <div className="card-body">
             <h5 className="card-title challenge-title">{this.state.title}</h5>
+            <div className="card-body">
+              <div>{this.state.numUsers} challengers</div>
+              {this.state.description}
+            </div>
             <FormBtn href="#" className="btn btn-primary join-btn ">
-                Join Challenge
-              </FormBtn>
-            <div className="row" />
+              Join Challenge
+            </FormBtn>
+            <hr />
+            <div className="workout-days">
+              {this.state.days &&
+                this.state.days.map(day => {
+                  return <p>{day}</p>;
+                })}
+            </div>
           </div>
         </div>
       </div>
