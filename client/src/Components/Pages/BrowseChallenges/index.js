c352998fe206f136ead6d79f3476735b56faedad
Lucas Merat <lucas.merat@gmail.com>
2019-05-13 11:50:00 -0500

    Cleaned up browse component to display challenges more clearly

--- client/src/Components/Pages/BrowseChallenges/index.js
+++ client/src/Components/Pages/BrowseChallenges/index.js
@@ -5,17 +5,19 @@ import ChallengeCard from "../ChallengeCard";
 export default function BrowseChallenges({ challenges }) {
   console.log(challenges);
   return (
-    challenges &&
-    challenges.map(challenge => {
-      return (
-        <ChallengeCard
-          key={challenge._id}
-          _id={challenge._id}
-          title={challenge.title}
-          description={challenge.description}
-          numUsers={challenge.user.length}
-        />
-      );
-    })
+    <div className="row">
+      {challenges &&
+        challenges.map(challenge => {
+          return (
+            <ChallengeCard
+              key={challenge._id}
+              _id={challenge._id}
+              title={challenge.title}
+              description={challenge.description}
+              numUsers={challenge.user.length}
+            />
+          );
+        })}
+    </div>
   );
 }
