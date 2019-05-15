084f8661cc144dbdf7b93e0dd0a0e31c2f2cf709
Felix Villalobos <felix.evl@gmail.com>
2019-05-15 10:01:38 -0500

    Removed console.logs

--- client/src/Components/Pages/DoneChallenges/index.js
+++ client/src/Components/Pages/DoneChallenges/index.js
@@ -4,14 +4,11 @@ import ChallengeCard from "../ChallengeCard";
 import "./DoneChallenges.css";
 
 export default function DoneChallegnes({ challenges }) {
-  console.log(challenges)
   return (
     <div className="row">
       {challenges &&
         challenges.map(challenge => {
           let endDate = moment(challenge.createdAt).add(30, "days");
-          console.log(endDate);
-          console.log(endDate.isBefore(moment()));
           if (endDate.isBefore(moment())) {
             return (
               <ChallengeCard
