ebe4bc2bc9c85f26c0d67422f2213d2eefac41b3
Dina Khouri <dina.khouri.91@gmail.com>
2019-05-23 12:17:22 -0500

    responsive profile

--- client/src/Components/AppComponents/Profile/ProfileNav/index.js
+++ client/src/Components/AppComponents/Profile/ProfileNav/index.js
@@ -50,7 +50,7 @@ function ProfileNav({ userName, profilePic,bio }) {
                   </div>
                   <div className="col-4">
                   <FormBtn className="edit-profile-btn">
-                    <Link className="nav-link" to="/profile/create">Create a Workout</Link>
+                    <Link className="nav-link" to="/profile/create">Create </Link>
                   </FormBtn>
                   </div>
                   </div>              
