9502f3405e4368f1d9220c2885c06b924d63b0d2
Lucas Merat <lucas.merat@gmail.com>
2019-05-29 10:36:23 -0500

    Slightly softer shadows. Remove duplicate CSS

--- client/src/Components/AppComponents/Auth/Login/index.js
+++ client/src/Components/AppComponents/Auth/Login/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect } from "react-router-dom";
 import { InstagramLoginButton, GoogleLoginButton } from "react-social-login-buttons";
 import { Col, Row, Container } from "../../../BootstrapComponents/Grid";
-import "./login.css";
+import "./Login.css";
 
 function Login(props) {
   let instaLink;
