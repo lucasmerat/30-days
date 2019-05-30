447415e5ce9862d6c28cc86cff9ca8340d507dad
Lucas Merat <lucas.merat@gmail.com>
2019-05-30 18:32:58 -0500

    Error messages for login

--- client/src/Components/BootstrapComponents/Footer/index.js
+++ client/src/Components/BootstrapComponents/Footer/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./style.css";
 
-function Footer(props) {
+function Footer() {
   return (
     <nav className="footer-nav page-footer footer-expand-lg footer-dark ">
-      <Link className="navbar-brand" to="/profile/browse">
-      <span className="footer-text" href="https://github.com/lucasmerat/Project-3" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> Code</i></span>
+      <a className="footer-text navbar-brand" href="https://github.com/lucasmerat/Project-3" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> Code</i></a>
        <p className="mt-3 mb-0 text-small copy-rights">© <span id="year">2019</span> 30Days, Co.</p>          
-      </Link>
     </nav>
   );
 }
