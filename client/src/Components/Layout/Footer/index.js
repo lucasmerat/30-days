de038a6ea7609207dee4674e32b7b967c67d9aea
Lucas Merat <lucas.merat@gmail.com>
2019-05-31 13:26:16 -0500

    Fix footer project link

--- client/src/Components/Layout/Footer/index.js
+++ client/src/Components/Layout/Footer/index.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <nav className="footer-nav page-footer footer-expand-lg footer-dark ">
-      <a className="footer-text navbar-brand" href="https://github.com/lucasmerat/Project-3" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> Code</i></a>
+      <a className="footer-text navbar-brand" href="https://github.com/lucasmerat/30-days" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"> Code</i></a>
        <p className="mt-3 mb-0 text-small copy-rights">© <span id="year">2019</span> 30Days, Co.</p>          
     </nav>
   );
